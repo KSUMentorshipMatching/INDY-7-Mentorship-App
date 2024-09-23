ba5b1289fc4e53abbacc774972c9deaeb7a497f7
Khang Nguyen <khangnnguyen02@gmail.com>
2024-09-23 18:37:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from flask import Flask,request,render_template,url_for
+from flask import Flask,request,render_template,url_for,redirect
 
 mydb = mysql.connector.connect(
     host = "indy7.cx8sgaa8o3lw.us-east-1.rds.amazonaws.com",
@@ -25,7 +25,7 @@ def bar():
     mycursor.execute(sql, val)
     mydb.commit()
 
-    return ("Data successfully submitted!")
+    return redirect(url_for('index'))
     
 
 if __name__ == "__main__":
