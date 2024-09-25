d26d18e9cd800807682556eaee5dda0e1c62b5de
Khang Nguyen <khangnnguyen02@gmail.com>
2024-09-25 17:29:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import mysql.connector
 from flask import Flask,request,render_template,url_for,redirect
 
+
+#Connects to the sql server. (TODO make it a getpass so that the information is not just sitting in the code. )
 mydb = mysql.connector.connect(
     host = "indy7.cx8sgaa8o3lw.us-east-1.rds.amazonaws.com",
     user = "admin",
@@ -8,14 +10,17 @@ mydb = mysql.connector.connect(
     database = "INDY7"
     )
 
-mycursor = mydb.cursor()
+#db cursor created to submit the form data.
+mycursor = mydb.cursor() 
 
 app = Flask(__name__)
 
+#Routes flask app to index.html that is in the templates folder
 @app.route("/")
 def index():
     return render_template("index.html")
 
+#Form handler that takes form data and submits it to the mysql database
 @app.route("/my_form", methods=["POST"])
 def bar():
     email = request.form["email"]
@@ -25,6 +30,7 @@ def bar():
     mycursor.execute(sql, val)
     mydb.commit()
 
+    #redirects the user back to index (TODO: make it redirect to a different page. )
     return redirect(url_for('index'))
     
 
